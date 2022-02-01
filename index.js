b0276b948e5a1ccf682936bbb9864e803688ce0e
etensor <dpenillac@correo.usbcali.edu.co>
2022-01-31 19:38:44 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// run `node index.js` in the terminal
+//import chalk from 'chalk';
+const chalk = require('chalk');
+const chalkAnimation = require('chalk-animation');
+const { faker } = require('@faker-js/faker');
+
+console.log('chalk arqj');
+
+let msg = [];
+let idx = 0;
+let datx = '';
+for (i = 0; i < 5; i++) {
+  datx = faker.hacker.phrase();
+  //  faker.animal.type();
+  msg.push(datx);
+}
+
+let printermsg = chalkAnimation.karaoke(chalk.blue(msg[idx]), 1.5);
+
+setInterval(() => {
+  printermsg.replace(msg[idx]);
+  idx += 1;
+  if (idx === msg.length) {
+    idx = 0;
+  }
+}, 2000);
